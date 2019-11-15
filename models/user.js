f76451d41b5b7613eda0fe2ac538ea03e6c26583
Karthic <karthic2507@gmail.com>
2019-11-15 19:15:18 +0530

    simple-auth-system

---
+++ models/user.js
@@ -0,0 +1,43 @@
+var Sequelize = require('sequelize');
+var bcrypt = require('bcrypt');
+
+// create a sequelize instance with our local postgres database information.
+var sequelize = new Sequelize('postgres://postgres@localhost:5432/auth-system');
+
+// setup User model and its fields.
+var User = sequelize.define('users', {
+    username: {
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull: false
+    },
+    email: {
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull: false
+    },
+    password: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+}, {
+    hooks: {
+      beforeCreate: (user) => {
+        const salt = bcrypt.genSaltSync();
+        user.password = bcrypt.hashSync(user.password, salt);
+      }
+    }
+});
+
+// create instance methods
+User.prototype.validPassword = (password, hash) => {
+    return bcrypt.compareSync(password, hash);
+};
+
+// create all the defined tables in the specified database.
+sequelize.sync()
+    .then(() => console.log('users table has been successfully created, if one doesn\'t exist'))
+    .catch(error => console.log('This error occured', error));
+
+// export User model for use in other files.
+module.exports = User;
